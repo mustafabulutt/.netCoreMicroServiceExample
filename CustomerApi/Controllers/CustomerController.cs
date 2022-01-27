4f70555546062090addcd58dafa67879b9facc8d
Mustafa Bulut <info@mbulut.com>
2022-01-27 17:40:54 +0300

    ++

---
+++ CustomerApi/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CustomerApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+
+			return new string[] { "müşteri1", "müşteri2" };
+
+		}
+
+	}
+}
---
+++ ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProductApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : ControllerBase
+	{
+
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get() {
+
+			return new string[] {"ürün1","ürün2" };
+		
+		}
+	}
+}
